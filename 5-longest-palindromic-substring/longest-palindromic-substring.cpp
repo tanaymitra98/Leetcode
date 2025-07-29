ff55e1296cace9ff04ce356136542e1a0519677c
tanaymitra98 <tanaymitra01@gmail.com>
2025-07-30 00:10:51 +0530

    Time: 858 ms (9.93%) | Memory: 9.4 MB (81.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,11 +1,10 @@
-bool isPalindrome(string &s, int left, int right){
+bool isPalindrome(const string &s, int left, int right){
     while(left<right){
         if(s[left] != s[right]){
             return false;
         }
         left++;
         right--;
-        
     }
     return true;
 }
@@ -17,17 +16,14 @@ public:
         int n = s.size();
         int maxlen = 1;
         int start =0;
-
-        for(int i =0 ; i<n; i++){//starting index
-            for(int j =i ; j<n; j++){//ending index
-                if(isPalindrome(s,i,j) && (j-i+1)>maxlen){
-                    start = i;
-                    maxlen = (j-i+1);
+        for(int i =0; i<n  ; i++){//starting index
+            for(int j =i; j<n ; j++){ //ending index
+                if(isPalindrome(s,i,j) && (j-i+1)>maxlen){//condition is to find the longest palindromic substring
+                    start = i; //update the starting index
+                    maxlen = (j-i+1); //update the max length each time 
                 }
             }
         }
         return s.substr(start,maxlen);
-
-        
     }
 };
