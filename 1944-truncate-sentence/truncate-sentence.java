3b4bff68d9be74ca2854769793eb5595adf56db4
tanaymitra98 <tanaymitra01@gmail.com>
2025-08-12 00:08:26 +0530

    Time: 2 ms (47.09%) | Memory: 42.3 MB (33.53%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String result[] = s.split( " " , k+1);
+        StringBuffer sb = new StringBuffer();
+
+        for(int i =0 ; i<k && i<result.length ; i++){
+            sb.append(result[i] + " ");
+        }
+        
+        return sb.toString().trim();
+    }
+}
